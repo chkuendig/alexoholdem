ee7465260a8e9958e0e7ec43e7390628c39a6530
ostrovsky.alex <ostrovsky.alex@63391347-8233-0410-beea-25b907642fa6>
2007-07-12 18:40:20 +0000

    fixed Eval5 refactorings.

--- trunk/ao/holdem/bots/MathBot.java
+++ trunk/ao/holdem/bots/MathBot.java
@@ -1,11 +1,16 @@
 package ao.holdem.bots;
 
-import ao.holdem.bots.util.*;
+import ao.holdem.bots.util.ApproximateOddFinder;
+import ao.holdem.bots.util.OddFinder;
+import ao.holdem.bots.util.Odds;
 import ao.holdem.def.bot.AbstractBot;
 import ao.holdem.def.model.cards.Hole;
 import ao.holdem.def.model.cards.community.Community;
 import ao.holdem.def.state.action.Action;
 import ao.holdem.def.state.env.Environment;
+
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 /**
  * Designed for heads up play.
@@ -41,4 +46,41 @@ public class MathBot extends AbstractBot
                 ? Action.RAISE_OR_CALL
                 : Action.CHECK_OR_FOLD;
     }
+
+
+    public static void main(String args[]) throws IOException
+    {
+        for (int i = 0; i < 10000; i++)
+        {
+            System.out.println();
+            System.out.println("=========================");
+            System.out.println("=========================");
+
+            System.out.println("to call:");
+            char toCallBytes[] = new char[20];
+            new InputStreamReader(System.in).read(toCallBytes);
+            String toCallStr  = new String(toCallBytes).trim();
+
+            System.out.println("pot size:");
+            char potSizeBytes[] = new char[20];
+            new InputStreamReader(System.in).read(potSizeBytes);
+            String potOddsStr = new String(potSizeBytes).trim();
+
+            double toCall  = Double.valueOf(toCallStr);
+            double potSize = Double.valueOf(potOddsStr);
+            double potOdds = toCall / (toCall + potSize);
+
+            System.out.println("-------------------");
+            System.out.println("potOdds: " + potOdds);
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
