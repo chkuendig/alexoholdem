5dbe2776e7010a91140061899864a0e9ec95060d
ostrovsky.alex <ostrovsky.alex@63391347-8233-0410-beea-25b907642fa6>
2007-07-30 09:13:56 +0000

    updated database mapping

--- ao/holdem/config/HoldemStarter.java
+++ ao/holdem/config/HoldemStarter.java
@@ -33,7 +33,7 @@ public class HoldemStarter
 
 //        injector.getInstance(HistoryTest.class).historyTest();
         injector.getInstance(IrcRunner.class).run(
-                "C:\\alex\\data\\irc_poker\\holdem\\199506");
+                "C:\\alex\\data\\irc_poker\\holdem\\199507");
 //        injector.getInstance(IrcRunner.class).run(
 //                "C:\\alex\\data\\limit_holdem\\hand_test");
     }
