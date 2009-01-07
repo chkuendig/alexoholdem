eb1dffb4e7ed38574449385910e1377b7cdef8f7
ostrovsky.alex <ostrovsky.alex@63391347-8233-0410-beea-25b907642fa6>
2009-01-07 18:13:29 +0000

    implemented incremental (tree-like) flop bucketing

--- trunk/ao/regret/holdem/JointBucketSequence.java
+++ trunk/ao/regret/holdem/JointBucketSequence.java
@@ -1,5 +1,9 @@
 package ao.regret.holdem;
 
+import ao.holdem.model.Round;
+import ao.holdem.model.card.Community;
+import ao.holdem.model.card.Hole;
+
 /**
  * User: iscott
  * Date: Jan 6, 2009
@@ -7,4 +11,32 @@ package ao.regret.holdem;
  */
 public class JointBucketSequence
 {
+    //--------------------------------------------------------------------
+
+
+    //--------------------------------------------------------------------
+    private final Hole      HOLE_FIRST;
+    private final Hole      HOLE_LAST;
+    private final Community COMMUNITY;
+
+
+    //--------------------------------------------------------------------
+    public JointBucketSequence(
+            Hole      firstToActHole,
+            Hole      lastToActHole,
+            Community community)
+    {
+        HOLE_FIRST = firstToActHole;
+        HOLE_LAST  = lastToActHole;
+        COMMUNITY  = community;
+    }
+
+
+    //--------------------------------------------------------------------
+    public HoldemBucket bucket(
+            boolean firstToAct,
+            Round   round)
+    {
+        return null;
+    }
 }
