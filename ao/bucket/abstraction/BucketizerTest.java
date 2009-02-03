5d4163fc77e55f4c77dd8720f862a168274e77f5
ostrovsky.alex <ostrovsky.alex@63391347-8233-0410-beea-25b907642fa6>
2009-02-03 03:49:40 +0000

    turned BucketOdds into an iterated operation, can perform calculations N at a time so as to avoid running out of memory (at the expense of extra runtime).

--- ao/bucket/abstraction/BucketizerTest.java
+++ ao/bucket/abstraction/BucketizerTest.java
@@ -6,7 +6,6 @@ import ao.bucket.abstraction.bucketize.BucketManager;
 import ao.bucket.abstraction.bucketize.BucketizerImpl;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 /**
  * Date: Oct 14, 2008
@@ -22,33 +21,33 @@ public class BucketizerTest
     //--------------------------------------------------------------------
     public static void main(String[] args) throws IOException
     {
-        double  holeBranch = 6,
-                flopBranch = 24,
-                turnBranch = 3,
-               riverBranch = 3;
+//        double  holeBranch = 6,
+//                flopBranch = 24,
+//                turnBranch = 3,
+//               riverBranch = 3;
+//
+//        if (args.length > 1)
+//        {
+//             holeBranch = Double.parseDouble(args[0]);
+//             flopBranch = Double.parseDouble(args[1]);
+//             turnBranch = Double.parseDouble(args[2]);
+//            riverBranch = Double.parseDouble(args[3]);
+//        }
+//
+//        System.out.println("Branching: " +
+//            Arrays.toString(new double[]{
+//                    holeBranch, flopBranch, turnBranch, riverBranch}));
+//
+//
+//        test((byte) (holeBranch),
+//             (char) (holeBranch * flopBranch),
+//             (char) (holeBranch * flopBranch * turnBranch),
+//             (char) (holeBranch * flopBranch * turnBranch * riverBranch));
 
-        if (args.length > 1)
-        {
-             holeBranch = Double.parseDouble(args[0]);
-             flopBranch = Double.parseDouble(args[1]);
-             turnBranch = Double.parseDouble(args[2]);
-            riverBranch = Double.parseDouble(args[3]);
-        }
-
-        System.out.println("Branching: " +
-            Arrays.toString(new double[]{
-                    holeBranch, flopBranch, turnBranch, riverBranch}));
-
-
-        test((byte) (holeBranch),
-             (char) (holeBranch * flopBranch),
-             (char) (holeBranch * flopBranch * turnBranch),
-             (char) (holeBranch * flopBranch * turnBranch * riverBranch));
-
-//        test((byte) (20),
-//             (char) (800),
-//             (char) (4800),
-//             (char) (28800));
+        test((byte) (20),
+             (char) (1134),
+             (char) (7430),
+             (char) (49263));
     }
 
 
