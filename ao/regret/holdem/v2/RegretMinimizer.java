6fe1431c1900b4dc1ea4213f4664e92c4712c146
ostrovsky.alex <ostrovsky.alex@63391347-8233-0410-beea-25b907642fa6>
2009-03-31 13:39:01 +0000

    * RegretMinimizer progress

--- ao/regret/holdem/v2/RegretMinimizer.java
+++ ao/regret/holdem/v2/RegretMinimizer.java
@@ -8,7 +8,6 @@ import ao.holdem.engine.state.tree.StateTree.Node;
 import ao.holdem.model.act.AbstractAction;
 import ao.regret.holdem.v2.InfoBranch.InfoSet;
 
-import java.util.Arrays;
 import java.util.Map;
 
 /**
@@ -43,19 +42,21 @@ public class RegretMinimizer
                 1.0, 1.0);
     }
 
-    private double approximate(
+
+    //--------------------------------------------------------------------
+    private Expectation approximate(
             StateTree.Node node,
             char           absDealerBuckets[],
             char           absDealeeBuckets[],
             double         pDealer,
             double         pDealee)
     {
-        boolean  canRaise      = node.state().canRaise();
-        boolean  dealerProp    = node.state().dealerIsNext();
-        double   expectedValue = 0;
-        double[] expectation   =
-                new double[ AbstractAction.VALUES.length ];
-        Arrays.fill(expectation, Double.NaN);
+        boolean       canRaise      = node.state().canRaise();
+        boolean       dealerProp    = node.state().dealerIsNext();
+        Expectation   expectedValue = new Expectation();
+        Expectation[] expectation   =
+                new Expectation[ AbstractAction.VALUES.length ];
+        //Arrays.fill(expectation, Double.NaN);
 
         InfoBranch infoBranch = INFO.info(
                 node.pathToFlop(), node.round());
@@ -71,31 +72,31 @@ public class RegretMinimizer
         for (Map.Entry<AbstractAction, Node> next : acts.entrySet())
         {
             double actProb = probabilities[ next.getKey().ordinal() ];
-            if (actProb == 0 /*&& info.isInformed()*/)
-            {
-                expectation[ next.getKey().ordinal() ] = 0;
+            if (actProb == 0 /*&& info.isInformed()*/) {
+                expectation[ next.getKey().ordinal() ] =
+                        new Expectation();
                 continue;
             }
 
-            double val;
-
+            Expectation    val;
             StateTree.Node nextNode = next.getValue();
             State          state    = nextNode.state();
             HeadsUpStatus  status   = state.headsUpStatus();
             if (status != HeadsUpStatus.IN_PROGRESS) {
-                double dealerNonLossProb =
-                        (status == HeadsUpStatus.SHOWDOWN)
-                        ? ODDS.nonLossProb(
-                                absDealerBuckets[3],
-                                absDealeeBuckets[3])
-                        : (status == HeadsUpStatus.DEALER_WINS)
-                        ? 1 : 0;
-                double toWin = ( dealerProp
-                               ? dealerNonLossProb
-                               : 1.0 - dealerNonLossProb);
-                val = state.pot().smallBlinds() * ((toWin - 0.5) * 2); // make commit based ?
+                if (status == HeadsUpStatus.SHOWDOWN) {
+                    val = new Expectation(state.stakes().smallBets(),
+                                          ODDS.nonLossProb(
+                                                 absDealerBuckets[3],
+                                                 absDealeeBuckets[3]));
+                } else {
+                    int dealeeCommit = state.seats(0).commitment().smallBlinds();
+                    int dealerCommit = state.seats(1).commitment().smallBlinds();
+
+                    val = new Expectation(dealerCommit, dealeeCommit,
+                                          (status == HeadsUpStatus.DEALER_WINS));
+                }
             } else {
-                val = -approximate(
+                val = approximate(
                             nextNode,
                             absDealerBuckets,
                             absDealeeBuckets,
@@ -104,7 +105,7 @@ public class RegretMinimizer
             }
 
             expectation[ next.getKey().ordinal() ] = val;
-            expectedValue += actProb * val;
+            expectedValue = expectedValue.plus(val.temper(actProb));
         }
 
         double[] counterfactualRegret =
@@ -112,12 +113,67 @@ public class RegretMinimizer
         for (AbstractAction act : acts.keySet())
         {
             double cRegret =
-                    (expectation[ act.ordinal() ] - expectedValue)
+                    (expectation[ act.ordinal() ].value(dealerProp) -
+                                    expectedValue.value(dealerProp))
                             * (dealerProp ? pDealee : pDealer);
             counterfactualRegret[ act.ordinal() ] = cRegret;
         }
         info.add(counterfactualRegret, canRaise);
 
         return expectedValue;
+    }
+
+
+    //--------------------------------------------------------------------
+    private static class Expectation
+    {
+        private final double dealer;
+        private final double dealee;
+
+        public Expectation()
+        {
+            this(0, 0);
+        }
+        private Expectation(double dealerExpect, double dealeeExpect)
+        {
+            dealer = dealerExpect;
+            dealee = dealeeExpect;
+        }
+
+        public Expectation(int    stakes,
+                           double dealerNonLossProb)
+        {
+            dealer = stakes * (dealerNonLossProb - 0.5) * 2.0;
+            dealee = -dealer;
+        }
+        
+        public Expectation(int     dealerCommit,
+                           int     dealeeCommit,
+                           boolean dealerWins)
+        {
+            if (dealerWins) {
+                dealer = dealeeCommit;
+                dealee = -dealer;
+            } else {
+                dealee = dealerCommit;
+                dealer = -dealee;
+            }
+        }
+
+        public double value(boolean forDealer)
+        {
+            return forDealer ? dealer : dealee;
+        }
+
+
+        public Expectation temper(double by)
+        {
+            return new Expectation(dealer * by, dealee * by);
+        }
+        public Expectation plus(Expectation addend)
+        {
+            return new Expectation(dealer + addend.dealer,
+                                   dealee + addend.dealee);
+        }
     }
 }
