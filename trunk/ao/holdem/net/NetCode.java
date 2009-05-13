91b08f83b651c2f2742395c26fd371d49ee5b51b
ostrovsky.alex <ostrovsky.alex@63391347-8233-0410-beea-25b907642fa6>
2009-05-13 00:49:50 +0000

    * NetCode progress

--- trunk/ao/holdem/net/NetCode.java
+++ trunk/ao/holdem/net/NetCode.java
@@ -4,27 +4,47 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
  */
 public class NetCode
 {
+    //--------------------------------------------------------------------
+    private static final int  IN_PORT = 7911;
+    private static final int OUT_PORT = 45067;
+
+
+    //--------------------------------------------------------------------
     public static void main(String args[]) throws Exception
     {
-        DatagramSocket readSocket  = new DatagramSocket(9978);
-        DatagramSocket writeSocket = new DatagramSocket(9979);
+        DatagramSocket readSocket  = new DatagramSocket( IN_PORT);
+        DatagramSocket writeSocket = new DatagramSocket(OUT_PORT);
 
         //for (int i = 0; i < 100000; i++)
         try
         {
+            Pattern ipPat = Pattern.compile(
+                    ".*#IP=(.+?)#.*");
+
             while (true)
             {
-                System.out.println("recieved: " + recieveString(readSocket));
+                System.out.println("awaiting input...");
+                String rec = recieveString(readSocket);
+                System.out.println("recieved: " + rec);
 
-    //            System.out.println("sending response.");
-    //            send( writeSocket );
+                Matcher matcher = ipPat.matcher(rec);
+                if (matcher.matches()) {
+                    String addr = matcher.group(1);
+                    System.out.println("Sending to " + addr);
 
+                    send(writeSocket, addr, "?INSTANCEID=0#ACTION=0");
+                } else {
+                    System.out.println(
+                            "unable to get address from: " + rec);
+                }
             }
         }
         finally
@@ -50,14 +70,17 @@ public class NetCode
     }
 
     private static void send(
-            DatagramSocket to) throws IOException
+            DatagramSocket to,
+            String         addressName,
+            String         message) throws IOException
     {
-        byte[] buf = "Hello World".getBytes();
+        byte[] buf = message.getBytes();
 
-        InetAddress    address = InetAddress.getByName("127.0.0.1");
+        InetAddress    address =
+                InetAddress.getByName(addressName); // "127.0.0.1"
         DatagramPacket packet  =
                 new DatagramPacket(buf, buf.length,
-                                   address, 4445);
+                                   address, OUT_PORT);
         to.send(packet);
     }
 
