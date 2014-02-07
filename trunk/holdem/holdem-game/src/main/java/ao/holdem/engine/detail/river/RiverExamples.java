bf15d6906e8d62f6e442a7eb608693a195e7afe4
ostrovsky.alex@gmail.com <ostrovsky.alex@gmail.com@63391347-8233-0410-beea-25b907642fa6>
2014-02-07 04:25:20 +0000

    * Refactoring.

---
+++ trunk/holdem/holdem-game/src/main/java/ao/holdem/engine/detail/river/RiverExamples.java
@@ -0,0 +1,62 @@
+package ao.holdem.engine.detail.river;
+
+import ao.holdem.model.canon.flop.Flop;
+import ao.holdem.model.canon.hole.CanonHole;
+import ao.holdem.model.canon.river.River;
+import ao.holdem.model.canon.turn.Turn;
+import ao.holdem.engine.detail.flop.FlopDetailFlyweight.CanonFlopDetail;
+import ao.holdem.engine.detail.flop.FlopDetails;
+import ao.holdem.engine.detail.turn.TurnRivers;
+import ao.holdem.model.enumeration.HandEnum;
+import ao.util.pass.Filter;
+import ao.util.pass.Traverser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Date: Jan 9, 2009
+ * Time: 12:44:39 PM
+ */
+public class RiverExamples
+{
+    //--------------------------------------------------------------------
+    public static List<River> examplesOf(long canonRiver)
+    {
+        final long acceptRiver = canonRiver;
+        final int  acceptTurn = TurnRivers.turnFor( acceptRiver );
+        CanonFlopDetail flopDetail = FlopDetails.containing(acceptTurn);
+        final int  acceptFlop = (int) flopDetail.canonIndex();
+        final int  acceptHole = (int) flopDetail.holeDetail().canonIndex();
+
+        final List<River> examples = new ArrayList<River>();
+        HandEnum.rivers(
+                new Filter<CanonHole>() {
+                    public boolean accept(CanonHole canonHole) {
+                        return canonHole.canonIndex() == acceptHole;
+                    }
+                },
+                new Filter<Flop>() {
+                    public boolean accept(Flop flop) {
+                        return flop.canonIndex() == acceptFlop;
+                    }
+                },
+                new Filter<Turn>() {
+                    public boolean accept(Turn turn) {
+                        return turn.canonIndex() == acceptTurn;
+                    }
+                },
+                new Filter<River>() {
+                    public boolean accept(River river) {
+                        return river.canonIndex() == acceptRiver;
+                    }
+                },
+                new Traverser<River>() {
+                    public void traverse(River river) {
+                        examples.add( river );
+                    }
+                }
+        );
+        return examples;
+    }
+}
---
+++ trunk/holdem/holdem-game/src/main/java/ao/holdem/engine/detail/turn/TurnOdds.java
@@ -0,0 +1,218 @@
+package ao.holdem.engine.detail.turn;
+
+import ao.Infrastructure;
+import ao.holdem.model.canon.turn.Turn;
+import ao.holdem.model.enumeration.HandEnum;
+import ao.holdem.engine.agglom.Odds;
+import ao.holdem.engine.agglom.impl.PreciseHeadsUpOdds;
+import ao.util.io.Dirs;
+import ao.util.pass.Traverser;
+import ao.util.persist.PersistentChars;
+import ao.util.persist.PersistentInts;
+import org.apache.log4j.Logger;
+
+import java.io.File;
+import java.util.Arrays;
+
+/**
+ * Date: Jan 14, 2009
+ * Time: 1:37:12 PM
+ */
+public class TurnOdds
+{
+    //--------------------------------------------------------------------
+    public static void main(String[] args)
+    {
+        long minWins = Integer.MAX_VALUE;
+        long maxWins = Integer.MIN_VALUE;
+
+        long minLose = Integer.MAX_VALUE;
+        long maxLose = Integer.MIN_VALUE;
+
+        long minTies = Integer.MAX_VALUE;
+        long maxTies = Integer.MIN_VALUE;
+
+        for (int i = 0; i < Turn.CANONS; i++)
+        {
+            Odds odds = lookup(i);
+
+            minWins = Math.min(minWins, odds.winOdds());
+            maxWins = Math.max(maxWins, odds.winOdds());
+
+            minLose = Math.min(minLose, odds.loseOdds());
+            maxLose = Math.max(maxLose, odds.loseOdds());
+
+            minTies = Math.min(minTies, odds.splitOdds());
+            maxTies = Math.max(maxTies, odds.splitOdds());
+        }
+
+        LOG.info("minWins = " + minWins);
+        LOG.info("maxWins = " + maxWins);
+
+        LOG.info("minLose = " + minLose);
+        LOG.info("maxLose = " + maxLose);
+
+        LOG.info("minTies = " + minTies);
+        LOG.info("maxTies = " + maxTies);
+    }
+
+
+    //--------------------------------------------------------------------
+    private static final Logger LOG =
+            Logger.getLogger(TurnOdds.class);
+
+    private static final char SENTINAL = Character.MAX_VALUE;
+
+
+    //--------------------------------------------------------------------
+    private static final File DIR =
+            Dirs.get(Infrastructure.path(
+                    "lookup/canon/detail/turn_odds/"));
+
+    private static final File FLAG_FILE  = new File(DIR,   "flag.int");
+    private static final File WIN_FILE   = new File(DIR,   "wins.char");
+    private static final File LOSE_FILE  = new File(DIR, "losses.char");
+    private static final File SPLIT_FILE = new File(DIR, "splits.char");
+
+
+    //--------------------------------------------------------------------
+    private TurnOdds() {}
+
+
+    //--------------------------------------------------------------------
+    private static final char[] WIN;
+    private static final char[] LOSE;
+    private static final char[] SPLIT;
+
+
+    //--------------------------------------------------------------------
+    static
+    {
+        LOG.debug("initializing TurnOdds");
+
+        char[] wins = PersistentChars.retrieve(WIN_FILE);
+        if (wins == null)
+        {
+            LOG.debug("starting from scratch");
+
+            WIN   = oddsComponent();
+            LOSE  = oddsComponent();
+            SPLIT = oddsComponent();
+        }
+        else
+        {
+            WIN   = wins;
+            LOSE  = PersistentChars.retrieve(LOSE_FILE);
+            SPLIT = PersistentChars.retrieve(SPLIT_FILE);
+        }
+
+        if (! getIsPreComputed())
+        {
+            LOG.debug("resuming computation");
+
+            computeOdds();
+            flush();
+            setIsPreComputed();
+        }
+    }
+
+    private static char[] oddsComponent()
+    {
+        char[] component = new char[ Turn.CANONS ];
+        Arrays.fill( component, SENTINAL );
+        return component;
+    }
+
+
+    //--------------------------------------------------------------------
+    private static void computeOdds()
+    {
+        LOG.debug("computing odds");
+
+        final int [] skipCount      = {0};
+        final int [] calcCount      = {0};
+        final long[] milestoneStart = {0};
+
+        HandEnum.uniqueTurns(
+                new Traverser<Turn>() {
+            public void traverse(Turn turn) {
+                if (WIN[ turn.canonIndex() ] != SENTINAL) {
+                    skipCount[0]++;
+                    return;
+                } else if (skipCount[0] != 0) {
+                    LOG.info("skipped " + skipCount[0]);
+                    skipCount[0] = 0;
+                }
+
+                Odds odds = PreciseHeadsUpOdds.INSTANCE.compute(
+                        turn.hole().reify(), turn.community());
+
+                char wins   = (char) odds.winOdds();
+                char losses = (char) odds.loseOdds();
+                char splits = (char) odds.splitOdds();
+
+                WIN  [ turn.canonIndex() ] = wins;
+                LOSE [ turn.canonIndex() ] = losses;
+                SPLIT[ turn.canonIndex() ] = splits;
+
+                checkpoint( calcCount[0]++, milestoneStart );
+            }});
+    }
+
+    private static void checkpoint(
+            int count, long[] milestoneStart)
+    {
+        if (milestoneStart[0] == 0)
+            milestoneStart[0] = System.currentTimeMillis();
+
+        if (count % 1000 == 0)
+            System.out.print(".");
+
+        boolean milesoneReached = ((count + 1) % 50000 == 0);
+        if (milesoneReached) {
+            System.out.println(
+                    " " + (count + 1) + ", completed 50,000 in " +
+                    (System.currentTimeMillis() - milestoneStart[0]));
+        }
+
+        if ((count + 1) % 1000000 == 0) flush();
+
+        if (milesoneReached)
+            milestoneStart[0] = System.currentTimeMillis();
+    }
+
+
+    //--------------------------------------------------------------------
+    public static Odds lookup(int canonTurnIndex)
+    {
+        return new Odds( WIN   [ canonTurnIndex ],
+                         LOSE  [ canonTurnIndex ],
+                         SPLIT [ canonTurnIndex ] );
+    }
+
+
+    //--------------------------------------------------------------------
+    private static boolean getIsPreComputed()
+    {
+        return PersistentInts.retrieve(FLAG_FILE) != null;
+    }
+
+    private static void setIsPreComputed()
+    {
+        PersistentInts.persist(new int[]{1}, FLAG_FILE);
+    }
+    
+
+    //--------------------------------------------------------------------
+    private synchronized static void flush()
+    {
+        long start = System.currentTimeMillis();
+
+        PersistentChars.persist(WIN,   WIN_FILE);
+        PersistentChars.persist(LOSE,  LOSE_FILE);
+        PersistentChars.persist(SPLIT, SPLIT_FILE);
+
+        System.out.println("\nflushed checkpoint, took " +
+                            (System.currentTimeMillis() - start));
+    }
+}
