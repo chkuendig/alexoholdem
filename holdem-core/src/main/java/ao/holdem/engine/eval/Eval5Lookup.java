5333d1b52e85bd62edc18d3faa38fd106c2b86a3
ostrovsky.alex@gmail.com <ostrovsky.alex@gmail.com@63391347-8233-0410-beea-25b907642fa6>
2014-04-19 22:16:21 +0000

    * Factoring out core.

---
+++ holdem-core/src/main/java/ao/holdem/engine/eval/Eval5Lookup.java
@@ -0,0 +1,48 @@
+package ao.holdem.engine.eval;
+
+/**
+ * See:
+ *  http://www.suffecool.net/poker/code/arrays.h
+ *  http://www.psenzee.com/code/fast_eval.c
+ */
+enum Eval5Lookup
+{;
+    //--------------------------------------------------------------------
+    public static short unique5(int index)
+    {
+        return Eval5Const.UNIQUE_5[ index ];
+    }
+
+    
+    //--------------------------------------------------------------------
+    public static short flushes(int index)
+    {
+        return Eval5Const.FLUSHES[ index ];
+    }
+    
+
+    //--------------------------------------------------------------------
+    public static short remainingHands(int primeProduct)
+    {
+        return Eval5Const.HASH_VALUES[ findFast(primeProduct) ];
+    }
+    private static int findFast(int u)
+    {
+        long adjusted = u;
+        adjusted = (adjusted + 0xe91aaa35) & 0xFFFFFFFFL;
+
+        adjusted ^= adjusted >> 16;
+
+        adjusted += ((adjusted << 8) & 0xFFFFFFFFL);
+        adjusted &= 0xFFFFFFFFL;
+
+        adjusted ^= adjusted >> 4;
+
+        long b  = (adjusted >> 8) & 0x1ff;
+        long a  = (((adjusted +
+                     ((adjusted << 2) & 0xFFFFFFFFL)
+                    ) & 0xFFFFFFFFL
+                   ) >> 19);
+        return (int)(a ^ Eval5Const.HASH_ADJUST[(int) b]);
+    }
+}
