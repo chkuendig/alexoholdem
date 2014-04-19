5e988d93bbdf585118ba609c48dce98788651715
ostrovsky.alex@gmail.com <ostrovsky.alex@gmail.com@63391347-8233-0410-beea-25b907642fa6>
2014-04-19 22:16:21 +0000

    * Factoring out core.

---
+++ trunk/holdem-core/src/main/java/ao/holdem/engine/eval/HandStrength.java
@@ -0,0 +1,11 @@
+package ao.holdem.engine.eval;
+
+/**
+ * 09/02/14 4:45 PM
+ */
+public enum HandStrength
+{;
+    public static final short LOWEST  = 0;
+    public static final short HIGHEST = 7461;
+    public static final short COUNT   = HIGHEST + 1;
+}
